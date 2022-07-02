e149be69d83cd4bfbb3b9a23591ba7294959f17f
Wendou90 <107042515+Wendou90@users.noreply.github.com>
2022-07-02 14:10:44 +0200

    Add files via upload

---
+++ CALCULAOR/script.js
@@ -0,0 +1,13 @@
+//arrays
+
+
+
+let myArray2=["what is the meaning life",42,true];
+console.log(myArray2[0]);
+//array declaration
+const numbers = [10,20,30,40,50];
+numbers.push(60,70,80);
+numbers.unshift(-30,-20,-10,0);
+for(let i=0;i<numbers.length;i++){
+    console.log(numbers[i]);
+}
